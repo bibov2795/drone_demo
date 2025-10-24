b7a095b10f7fef841bc131ad22e242a7ea9dfe41
bibov2795 <bibov2795@gmail.com>
2025-10-24 16:30:48 +0800

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -1,16 +1,25 @@
 pluginManagement {
+    // 读取 flutterSdkPath，从 gradle.properties 或环境变量注入
+    val flutterSdkPath = extra["flutterSdkPath"]?.toString()
+        ?: System.getenv("FLUTTER_ROOT")
+        ?: throw GradleException("Missing flutterSdkPath or FLUTTER_ROOT")
+
+    includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
+
     repositories {
         google()
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
     }
 }
 
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://storage.googleapis.com/download.flutter.io") }
     }
 }
 
