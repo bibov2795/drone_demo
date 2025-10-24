df6356412a7a0286bb69eab1b2e6259cc55b1103
bibov2795 <bibov2795@gmail.com>
2025-10-24 16:12:53 +0800

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,11 +1,18 @@
+// android/build.gradle.kts
+
 rootProject.buildDir = file("../build")
 
 subprojects {
     buildDir = file("${rootProject.buildDir}/${name}")
     evaluationDependsOn(":app")
+
+    // 🔹 确保子模块也继承仓库配置（部分 Flutter 插件会依赖）
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.buildDir)
 }
-
